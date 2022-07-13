ed24659ad588f6a6f3985935245b4e58e99934c7
Alberto Aguilera <drimer.aaj@gmail.com>
2022-07-13 17:22:31 +0100

    add healthcheck endpoint

--- src/app.py
+++ src/app.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def health_check():
+    return '', 204
+
+
 @app.route('/<param>/')
 def hello(param: str):
     print(f'Responding to {param}')
